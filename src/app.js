d17c245c5ecf7e7a42671adae1abdc1f23b2724e
Fabiola Granados <faby.granados@gmail.com>
2023-01-26 18:01:59 -0800

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day}  ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
     console.log(response.data);
     let cityElement = document.querySelector("#city");
@@ -5,11 +15,13 @@ function displayTemperature(response) {
     let descriptionElement= document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
     temperatureElement.innerHTML = Math.round (response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML= response.data.temperature.humidity;
     windElement.innerHTML = Math.round (response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.time * 1000)
 }
 
 let apiKey="0406a2d65ft65fe83b49bc397b79bo2a";
