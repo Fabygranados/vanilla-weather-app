0f09cfe49b16dc0168c2463b2ca48654e8fe8b6c
Fabiola Granados <faby.granados@gmail.com>
2023-01-27 10:17:12 -0800

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -15,16 +15,20 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+
     temperatureElement.innerHTML = Math.round (response.data.temperature.current);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML= response.data.temperature.humidity;
     windElement.innerHTML = Math.round (response.data.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.time * 1000)
+    dateElement.innerHTML = formatDate(response.data.time * 1000);
+    iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`) ;
+    iconElement.setAttribute("alt", response.data.condition.description)
 }
 
 let apiKey="0406a2d65ft65fe83b49bc397b79bo2a";
-let city = "montreal";
+let city = "paris";
 let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=0406a2d65ft65fe83b49bc397b79bo2a&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
