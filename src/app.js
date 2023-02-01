d6278ee7f167f30a09655e40b6f94fbe27cb9f67
Fabiola Granados <faby.granados@gmail.com>
2023-02-01 13:17:09 -0800

    create HTML in JS

--- src/app.js
+++ src/app.js
@@ -7,6 +7,35 @@ function formatDate(timestamp){
     return `${day}  ${hours}:${minutes}`;
 }
 
+    function displayForecast() {
+        let forecastElement = document.querySelector("#forecast");
+
+        let forecastHTML = `<div class="row">`;
+        let days = ["Thu", "Fri", "Sat", "Sun"];
+        days.forEach(function(day) {
+           forecastHTML = 
+            forecastHTML + 
+            `
+            <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
+                    alt=""
+                    width="45"
+                />
+                <div class="weather-forecast-temperature">
+                    <span class="weather-forecast-temperature-max"> 18° </span>
+                    <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+            </div>
+        `;
+        })
+        
+        forecastHTML = forecastHTML + `</div>`
+        forecastElement.innerHTML = forecastHTML;
+
+        
+    }
 
 function displayTemperature(response) {
     let cityElement = document.querySelector("#city");
@@ -73,3 +102,4 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
 search("New York");
+displayForecast(); 
