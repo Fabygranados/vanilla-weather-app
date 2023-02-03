5785ac8fd2ab20f75a68092ff24dc524dba4df57
Fabiola Granados <faby.granados@gmail.com>
2023-02-03 11:23:10 -0800

    Remove the F° conversion

--- src/app.js
+++ src/app.js
@@ -100,34 +100,4 @@ function handleSubmit(event){
     search(cityInputElement.value);
 }
 
-function displayFahrenheitTemperature(event) {
-    event.preventDefault();
-    let fahrenhaitTemperature = (celsiusTemperature * 9 / 5) + 32;
-    let temperatureElement = document.querySelector("#temperature");
-
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    temperatureElement.innerHTML = Math.round(fahrenhaitTemperature);
-}
-function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
-
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature); 
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature); 
-
-
 search("New York");
